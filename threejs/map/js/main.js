1f9e52a6fd54b3ab122866cb2ee64a5ead2deb46
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2019-06-23 20:54:08 +0900

    [mod] map

--- threejs/map/js/main.js
+++ threejs/map/js/main.js
@@ -4,8 +4,8 @@
 (function() {
 
     // グローバル変数
-    var camera, scene, renderer, controls
-    var mesh = []
+    var camera, scene, renderer, controls, alpha = 0, beta = 0, gamma = 0
+    var context = canvas.getContext("2d");
 
     // Element
     var elCanvas = document.querySelector('#canvas')
@@ -31,6 +31,13 @@
         renderer.setPixelRatio(window.devicePixelRatio)
         renderer.setSize(window.innerWidth, window.innerHeight)
         window.addEventListener('resize', onWindowResize)
+
+        // ジャイロセンサの値が変化したら実行される deviceorientation イベント
+        window.addEventListener("deviceorientation", (dat) => {
+            alpha = dat.alpha;  // z軸（表裏）まわりの回転の角度（反時計回りがプラス）
+            beta  = dat.beta;   // x軸（左右）まわりの回転の角度（引き起こすとプラス）
+            gamma = dat.gamma;  // y軸（上下）まわりの回転の角度（右に傾けるとプラス）
+        });
     }
 
     /**
@@ -56,8 +63,43 @@
         // コントローラー更新
         controls.update()
 
+        displayData();
+        drawOrientation();
+
         renderer.render(scene, camera)
         window.requestAnimationFrame(render)
+    }
+
+
+    function displayData() {
+        var txt = document.getElementById("txt");   // データを表示するdiv要素の取得
+        txt.innerHTML = "alpha: " + alpha + "<br>"  // x軸の値
+                    + "beta:  " + beta  + "<br>"  // y軸の値
+                    + "gamma: " + gamma;          // z軸の値
+    }
+        
+    /**
+     * コンパスのような絵を描く
+     */
+    function drawOrientation() {
+        var centerX = canvas.width  / 2;            // canvasの中心のX座標
+        var centerY = canvas.height / 2;	        // canvasの中心のY座標
+        var radius  = 100;                          // 枠円の半径および針の長さ
+        var radianAlpha = alpha * Math.PI / 180;    // 角度をラジアンに変換
+        context.clearRect(0, 0, canvas.width, canvas.height);   // canvasの内容を消す clearRect(x, y, w, h)
+        context.beginPath();                        // 描画開始
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI);  // 枠円を描く
+        context.strokeStyle = "rgb(0, 0, 0)";       // 枠円の線の色
+        context.lineWidth = 2;                      // 線の太さ
+        context.stroke();                           // 線を描画
+        context.beginPath();                        // 描画開始
+        context.moveTo(centerX, centerY);           // 中心に移動
+        // 線を引く（cosでx座標、sinでy座標が得られる。長さradiusを掛ける。-90度すると真上に向く。）
+        context.lineTo(centerX + Math.cos(radianAlpha - Math.PI / 2) * radius,
+                    centerY + Math.sin(radianAlpha - Math.PI / 2) * radius);
+        context.strokeStyle = "rgb(255, 0, 0)";     // 針の線の色
+        context.lineWidth = 5;                      // 線の太さ
+        context.stroke();                           // 線を描画
     }
 
     // 処理開始
